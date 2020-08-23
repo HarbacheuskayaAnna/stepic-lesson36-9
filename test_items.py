7e6b42b9b17678bfbce5cd45117bd0817ba007f1
HarbacheuskayaAnna <harbacheuskaya.anna@gmail.com>
2020-08-23 13:23:35 +0300

    Add updated files conftest.py and test_items.py with the result of the task for peer-review

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 
 
-def test_guest_should_see_login_link(browser):
+def test_check_button_is_on_the_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
